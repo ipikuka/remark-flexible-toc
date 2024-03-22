db4e99577f2afeee943766283fb182e6e67629a1
“talatkuyuk” <talatkuyuk@gmail.com>
2024-03-23 00:58:02 +0300

    added a test util and updated all the tests

---
+++ tests/util/index.ts
@@ -0,0 +1,25 @@
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import gfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeFormat from "rehype-format";
+import rehypeStringify from "rehype-stringify";
+import type { VFileCompatible, VFile } from "vfile";
+
+import plugin, { type FlexibleTocOptions } from "../../src";
+
+const compilerCreator = (options?: FlexibleTocOptions) =>
+  unified()
+    .use(remarkParse)
+    .use(gfm)
+    .use(plugin, options)
+    .use(remarkRehype)
+    .use(rehypeFormat)
+    .use(rehypeStringify);
+
+export const process = async (
+  content: VFileCompatible,
+  options?: FlexibleTocOptions,
+): Promise<VFile> => {
+  return compilerCreator(options).process(content);
+};
